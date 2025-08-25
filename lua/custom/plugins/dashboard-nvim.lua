3a0c5036b7ad522c5dea9fb59780105322a805f0
Zawisza-Czarny <matisern@gmail.com>
2025-08-25 08:58:36 +0000

    zmiana loga

--- lua/custom/plugins/dashboard-nvim.lua
+++ lua/custom/plugins/dashboard-nvim.lua
@@ -3,13 +3,10 @@ return {
   lazy = false, -- As https://github.com/nvimdev/dashboard-nvim/pull/450, dashboard-nvim shouldn't be lazy-loaded to properly handle stdin.
   opts = function()
     local logo = [[
-         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
-         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
-         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
-         ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
-         ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
-         ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
-    ]]
+ /\_/\
+  ( o.o )
+ > ^ <
+      ]]
 
     logo = string.rep('\n', 8) .. logo .. '\n\n'
 
